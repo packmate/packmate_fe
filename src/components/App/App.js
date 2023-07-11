1cf85577146e1a81cd05e7ad45b4f43993155fa5
Rachel Brendel <brendelrachel@gmail.com>
2023-07-11 12:25:10 -0500

    Add error page redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Error from '../Error/Error'
 import Header from '../header/Header'
-import { Route, Switch } from 'react-router-dom'
+import ReactRouterDom, { Route, Switch, Redirect } from 'react-router-dom'
 import Home from '../Home/Home'
 import SavedPage from '../SavedPage/savedPage'
 import ListPage from '../ListPage/ListPage'
@@ -15,20 +15,26 @@ class App extends React.Component {
     }
   }
 
+  componentDidCatch(error) {
+    this.setState({ error: error.toString() });
+  }
+
   render() {
+    const { error } = this.state;
+
     return (
       <main className='main'>
         <Header />
         <Switch>
-          <Route exact path='*' render={() => <Home />} />
-          {/* <Route exact path='/items' render={ } /> */}
-          {/* <Route exact path='/user-lists' render={ } /> */}
-          {/* <Route exact path='*' render={() => <SavedPage />} /> */}
+          <Route exact path='/' component={Home} />
+          {/* <Route exact path='/createlist' component={ListPage} /> */}
+          <Route exact path='/mylist' component={SavedPage} />
+          <Route path='/error' render={() => <Error error={error} />} />
+          <Redirect to='/error' />
         </Switch>
       </main>
     );
   }
-
 }
 
 export default App
