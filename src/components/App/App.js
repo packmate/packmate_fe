c69849293b06e012cea40d6c48cb38f28c61f24e
Rachel Brendel <brendelrachel@gmail.com>
2023-07-11 12:01:55 -0500

    Add home page as the landing for our app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Header from '../header/Header'
 import { Route, Switch } from 'react-router-dom'
 import Home from '../Home/Home'
 import SavedPage from '../SavedPage/savedPage'
+import ListPage from '../ListPage/ListPage'
 
 class App extends React.Component {
   constructor() {
@@ -15,25 +16,19 @@ class App extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <main className='main'>
         <Header />
         <Switch>
-          <Route exact path='/' render={ () => {
-            <Home />} }
-          />
-          {/* <Route exact path='/items' render={ }
-          /> */}
-          {/* <Route exact path='/user-lists' render={ }
-          /> */}
-           
-          <Route exact path='*' render={ () => 
-            <Error error={this.state.error}/> }
-          /> 
+          <Route exact path='*' render={() => <Home />} />
+          {/* <Route exact path='/items' render={ } /> */}
+          {/* <Route exact path='/user-lists' render={ } /> */}
+          {/* <Route exact path='*' render={() => <SavedPage />} /> */}
         </Switch>
       </main>
-    ) 
+    );
   }
+
 }
 
 export default App
