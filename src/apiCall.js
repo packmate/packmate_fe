5a797b3d7ec6036979fafc98cb2780424a29b039
Kelli Watkins <watkinskellil@gmail.com>
2023-07-12 11:05:57 -0600

    Address actions errors

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,10 +1,9 @@
-import { useQuery, useMutation } from '@apollo/client'
+//import { useQuery, useMutation } from '@apollo/client'
 
 const fetchItems = async (tripSelection) => {
   const response = await fetch('https://packmate-be-d3fb267b5fee.herokuapp.com/graphql', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    headers: { 'Access-Control-Allow-Credentials': 'true' },
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Credentials': 'true' },
     body: JSON.stringify({
       query: `query{
               items(category: "${tripSelection}") {
