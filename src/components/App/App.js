48f91ec527f22e765002d7189d2144a358fe2e0e
Rachel Brendel <brendelrachel@gmail.com>
2023-07-12 23:28:12 -0500

    Continue css styling and add functionality to get the saved list from list page without name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,41 +9,131 @@ import ListPage from '../ListPage/ListPage'
 import fetchItems from '../../apiCall'
 import { useQuery, useMutation } from '@apollo/client'
 
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       error: "",
+//       tripSelection: "",
+//       packItems: [],
+//       selectedItems: [],
+//       savedLists: [],
+//     };
+//     this.handleSaveList = this.handleSaveList.bind(this);
+//   }
+
+//   createList = () => {
+
+//     const { tripSelection } = this.state;
+//     fetchItems(tripSelection)
+//       .then(data => {
+
+//         this.setState({ packItems: data.data.items })
+//       })
+//       .catch(error => {
+//         this.setState({
+//           error: error.message
+//         })
+//       })
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.packItems !== this.state.packItems) {
+//       this.props.history.push('/lists')
+//     }
+ 
+//   }
+
+//   onChange = (event) => {
+//     this.setState({ tripSelection: event.target.value })
+//   }
+
+//   componentDidCatch(error) {
+//     this.setState({ error: error.toString() });
+//   }
+
+//   handleCheckboxChange = (itemId) => {
+//     this.setState((prevState) => {
+//       const { selectedItems } = prevState;
+//       if (selectedItems.includes(itemId)) {
+//         return { selectedItems: selectedItems.filter((id) => id !== itemId) }
+//       } else {
+//         return { selectedItems: [...selectedItems, itemId] }
+//       }
+//     })
+//   }
+
+//   handleSaveList(listName, listItems) {
+//     this.setState((prevState) => ({
+//       savedLists: [...prevState.savedLists, { name: listName, items: listItems }],
+//     }));
+//   }
+
+//   render() {
+//     const { error, packItems, selectedItems } = this.state;
+
+
+//     return (
+//       <main className='main'>
+//         <Header />
+//         <Switch>
+//           <Route exact path='/' component={() =>
+//             <Home onChange={this.onChange} createList={this.createList} value={this.state.tripSelection} />}
+
+//           />
+//           <Route exact path='/lists' component={() =>
+//             <ListPage
+//               packItems={packItems}
+//               selectedItems={selectedItems}
+//               fetchItems={this.fetchItems}
+//               handleCheckboxChange={this.handleCheckboxChange}
+//               handleSaveList={this.handleSaveList}
+//             />}
+//           />
+
+//           <Route exact path="/mylist" component={() => <SavedPage savedLists={this.state.savedLists} />} />
+//           <Route path='*' render={() => <Error error={error} />} />
+//         </Switch>
+//       </main>
+//     );
+//   }
+// }
+
+// export default withRouter(App)
+
+
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       error: "",
       tripSelection: "",
       packItems: [],
-      selectedItems: []
-    }
+      selectedItems: [],
+      savedLists: [],
+    };
+    this.handleSaveList = this.handleSaveList.bind(this);
   }
 
   createList = () => {
-
     const { tripSelection } = this.state;
     fetchItems(tripSelection)
       .then(data => {
-
-        this.setState({ packItems: data.data.items })
+        this.setState({ packItems: data.data.items });
       })
       .catch(error => {
-        this.setState({
-          error: error.message
-        })
-      })
+        this.setState({ error: error.message });
+      });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.packItems !== this.state.packItems) {
-      this.props.history.push('/lists')
+      this.props.history.push('/lists');
     }
- 
   }
 
   onChange = (event) => {
-    this.setState({ tripSelection: event.target.value })
+    this.setState({ tripSelection: event.target.value });
   }
 
   componentDidCatch(error) {
@@ -54,19 +144,30 @@ class App extends React.Component {
     this.setState((prevState) => {
       const { selectedItems } = prevState;
       if (selectedItems.includes(itemId)) {
-        return { selectedItems: selectedItems.filter((id) => id !== itemId) }
+        return { selectedItems: selectedItems.filter((id) => id !== itemId) };
       } else {
-        return { selectedItems: [...selectedItems, itemId] }
+        return { selectedItems: [...selectedItems, itemId] };
       }
-    })
+    });
   }
 
+  handleSaveList = (listName, listItems) => {
+    const { savedLists } = this.state;
+    const newList = {
+      name: listName,
+      items: listItems.map((itemId) => {
+        const item = this.state.packItems.find((item) => item.id === itemId);
+        return { ...item, packed: false };
+      }),
+    };
+    this.setState((prevState) => ({
+      savedLists: [...prevState.savedLists, newList],
+    }));
+  };
+  
 
   render() {
-    const { error } = this.state;
-
-    const { packItems, selectedItems } = this.state;
-
+    const { error, packItems, selectedItems, savedLists } = this.state;
 
     return (
       <main className='main'>
@@ -74,7 +175,6 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={() =>
             <Home onChange={this.onChange} createList={this.createList} value={this.state.tripSelection} />}
-
           />
           <Route exact path='/lists' component={() =>
             <ListPage
@@ -82,11 +182,11 @@ class App extends React.Component {
               selectedItems={selectedItems}
               fetchItems={this.fetchItems}
               handleCheckboxChange={this.handleCheckboxChange}
+              handleSaveList={this.handleSaveList}
             />}
           />
-
-
-          <Route exact path='/mylist' component={SavedPage} />
+          <Route exact path="/mylist" component={() =>
+            <SavedPage savedLists={savedLists} />} />
           <Route path='*' render={() => <Error error={error} />} />
         </Switch>
       </main>
@@ -94,4 +194,4 @@ class App extends React.Component {
   }
 }
 
-export default withRouter(App)
+export default withRouter(App);
--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom'
+import './ListPage.css'
 
-const ListPage = ({ packItems, selectedItems, fetchItems, handleCheckboxChange }) => {
-
+const ListPage = ({ packItems, selectedItems, fetchItems, handleCheckboxChange, handleSaveList }) => {
     const history = useHistory();
 
-    const makeList = packItems.map(item => {
-
-        return (
-            <div key={item.id}>
-                <input
-                    type='checkbox'
-                    checked={selectedItems.includes(item.id)}
-                    onChange={(e) => handleCheckboxChange(item.id)}
-                />
-                <span>{item.name}</span>
-            </div>
-        )
-    })
-
     const handleButtonClick = () => {
+        const listNumber = 1;
+        const listName = `List ${listNumber}`;
+        handleSaveList(listName, selectedItems);
         history.push('/mylist');
-    };
+      };
 
     return (
-        <div>
-
-            {makeList}
-            <button onClick={handleButtonClick}>Submit</button>
-
+        <div className='list-page-container'>
+            <div className='list-name-container'>
+                {packItems.map(item => (
+                    <div key={item.id}>
+                        <input
+                            type='checkbox'
+                            checked={selectedItems.includes(item.id)}
+                            onChange={() => handleCheckboxChange(item.id)}
+                        />
+                        <span className='item-name'>{item.name}</span>
+                    </div>
+                ))}
+            </div>
+            <button className='submit-list-button' onClick={handleButtonClick}>Submit</button>
         </div>
-    )
+    );
 };
 
 export default ListPage;
-
-
