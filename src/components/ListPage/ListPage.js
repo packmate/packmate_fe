22d0f725918bf5961ff5508a7e5bd0a28d9cb268
Kelli Watkins <watkinskellil@gmail.com>
2023-07-14 17:19:50 -0600

    Fix list page errors

--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -1,13 +1,9 @@
-
 import React, { useState } from "react";
 import "./ListPage.css";
 import { useHistory } from "react-router-dom";
 import PropTypes from 'prop-types';
 
-
 const ListPage = ({
-
-
     packItems,
     selectedItems,
     handleCheckboxChange,
@@ -17,103 +13,52 @@ const ListPage = ({
     resetState,
     formValid
 }) => {
-
     const history = useHistory();
     const [customItem, setCustomItem] = useState("");
     const [customItems, setCustomItems] = useState([]);
 
-    const handleButtonClick = () => {
-    const allItems = [...selectedItems, ...customItems]; // Combine packItems and customItems
-    console.log("All Items:", allItems);
-    handleSaveList(listName, selectedItems);
-    history.push("/mylist");
-    resetState();
-  };
 
+    const handleButtonClick = () => {
+        const allItems = [...selectedItems, ...customItems]; // Combine packItems and customItems
+        console.log("All Items:", allItems);
+        handleSaveList(listName, selectedItems);
+        history.push("/mylist");
+        resetState();
+    };
 
     const handleAddCustomItem = () => {
-    if (customItem.trim() !== "") {
-      const newItem = {
-        id: Math.random().toString(36).substring(7),
-        name: customItem.trim(),
-        packed: false,
-      };
-      setCustomItems([...customItems, newItem]);
-      setCustomItem("");
-      handleCheckboxChange(newItem.id);
-    }
-  };
+        if (customItem.trim() !== "") {
+            const newItem = {
+                id: Math.random().toString(36).substring(7),
+                name: customItem.trim(),
+                packed: false,
+            };
+            setCustomItems([...customItems, newItem]);
+            setCustomItem("");
+            handleCheckboxChange(newItem.id);
+        }
+    };
 
+    const displayCustomItems = () => {
+        return customItems.map((item) => (
+            <div key={item.id}>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={selectedItems.includes(item.id)}
+                        onChange={() => handleCheckboxChange(item.id)}
+                    />
+                    <span>{item.name}</span>
+                </label>
+            </div>
+        ));
+    };
 
+    const allItems = [...packItems, ...customItems]; // Combine packItems and customItems
 
-const displayCustomItems = () => {
-    return customItems.map((item) => (
-      <div key={item.id}>
-        <label>
-          <input
-            type="checkbox"
-            checked={selectedItems.includes(item.id)}
-            onChange={() => handleCheckboxChange(item.id)}
-          />
-          <span>{item.name}</span>
-        </label>
-      </div>
-    ));
-  };
-
-
-  const allItems = [...packItems, ...customItems]; // Combine packItems and customItems
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={listName}
-        onChange={handleNameChange}
-        placeholder="Enter List Name"
-        className="list-name-input"
-      />
-      <div className="list-name-container">
-        {allItems.map((item) => (
-          <div key={item.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={selectedItems.includes(item.id)}
-                onChange={() => handleCheckboxChange(item.id)}
-              />
-              <span className="item-name">{item.name}</span>
-            </label>
-          </div>
-        ))}
+    return (
         <div>
-          <input
-            type="text"
-
-            value={listName}
-            onChange={handleNameChange}
-            placeholder="Enter List Name"
-            className="list-name-input"
-        />
-        <div className="list-name-container">
-            {packItems.map((item) => (
-                <div key={item.id}>
-
-
-                <input
-                    type="checkbox"
-                    checked={selectedItems.includes(item)}
-                    onChange={() => handleCheckboxChange(item)}
-                />
-
-
-                <span className="item-name">{item.name}</span>
-                </div>
-            ))}
-            {displayCustomItems()}
-            <div>
-                <input
-
+            <input
                 type="text"
                 value={listName}
                 onChange={handleNameChange}
@@ -121,17 +66,18 @@ const displayCustomItems = () => {
                 className="list-name-input"
             />
             <div className="list-name-container">
-                {packItems.map((item) => (
+                {allItems.map((item) => (
                     <div key={item.id}>
-                        <input
-                            type="checkbox"
-                            checked={selectedItems.includes(item.id)}
-                            onChange={() => handleCheckboxChange(item.id)}
-                        />
-                        <span className="item-name">{item.name}</span>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={selectedItems.includes(item.id)}
+                                onChange={() => handleCheckboxChange(item.id)}
+                            />
+                            <span className="item-name">{item.name}</span>
+                        </label>
                     </div>
                 ))}
-                {displayCustomItems()}
                 <div>
                     <input
                         type="text"
@@ -140,17 +86,25 @@ const displayCustomItems = () => {
                         placeholder="Enter custom item"
                         className="custom-item-input"
                     />
-                    <button className="add-item" onClick={handleAddCustomItem}>Add Item</button>
+                    <button className="add-item" onClick={handleAddCustomItem}>
+                        Add Item
+                    </button>
                 </div>
             </div>
-            {!formValid && (<p className="invalid-form">Please fill out the list name and select at least one item!</p>)}
-
-        <button disabled={!formValid} onClick={handleButtonClick} className="submit-list-button">Submit</button>
-    </div>
-
-
+            {!formValid && (
+                <p className="invalid-form">
+                    Please fill out the list name and select at least one item!
+                </p>
+            )}
+            <button
+                disabled={!formValid}
+                onClick={handleButtonClick}
+                className="submit-list-button"
+            >
+                Submit
+            </button>
+        </div>
     );
-
 };
 
 ListPage.propTypes = {
@@ -164,16 +118,6 @@ ListPage.propTypes = {
     formValid: PropTypes.bool.isRequired,
 };
 
-  ListPage.propTypes = {
-    packItems: PropTypes.array.isRequired,
-    selectedItems: PropTypes.array.isRequired,
-    handleCheckboxChange: PropTypes.func.isRequired,
-    handleNameChange: PropTypes.func.isRequired,
-    listName: PropTypes.string.isRequired,
-    handleSaveList: PropTypes.func.isRequired,
-    resetState: PropTypes.func.isRequired,
-    formValid: PropTypes.bool.isRequired,
-};
-
 export default ListPage;
+
 
